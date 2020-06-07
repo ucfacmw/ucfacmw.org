bf671a189526efa4992fef0fa070f7d06dbc2d6b
h3rmi0n3 <chloegeller@knights.ucf.edu>
2020-06-06 20:32:01 -0400

    got vanta-birds working

---
+++ static/js/vanta-birds-config.js
@@ -0,0 +1,20 @@
+VANTA.BIRDS({
+    el: "#vanta-birds-js",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color1: 0x4ebe,
+    color2: 0xd4a200,
+    colorMode: "lerpGradient",
+    birdSize: 2.80,
+    separation: 82.00,
+    alignment: 17.00,
+    cohesion: 4.00
+  })
+  
+
+
+// ! ASK JOHN ABOUT /home/index.md 
